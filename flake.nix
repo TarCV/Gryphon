7fc809fa85a76db92d340a8041d3d92bd379a1c2
TarCV <2857607+TarCV@users.noreply.github.com>
2022-06-06 01:14:18 +0300

    Try using the latest hydra revision with successful Swift package

--- flake.nix
+++ flake.nix
@@ -1,12 +1,15 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs/release-21.11";
-    swift.url = "github:dduan/swift-builders";
-    swiftPm.url = "github:dduan/nixSwiftPM";
+    nixpkgs.url = "nixpkgs/007ccf2f4f1da567903ae392cbf19966eb30cf20";
+    swiftPm = {
+      url = "github:dduan/nixSwiftPM";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.swift.follows = "nixpkgs";
+    };
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, swift, swiftPm }:
+  outputs = { self, nixpkgs, flake-utils, swiftPm }:
     (flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
